e10bddb2913949432398c3602d4a2f9d5ebcd473
Ethan Hann <ethanhann@gmail.com>
2015-05-18 17:01:30 -0400

    Add update callback option

--- src/eeh-flot-directive.js
+++ src/eeh-flot-directive.js
@@ -1,17 +1,26 @@
 (function (angular) {
     'use strict';
 
-    function FlotDirective(eehFlot) {
+    function FlotDirective(eehFlot, $interval) {
         return {
             restrict: 'AE',
             template: '<div class="eeh-flot"></div>',
             scope: {
                 dataset: '=',
-                options: '@'
+                options: '@',
+                updateCallback: '&',
+                updateCallbackTime: '@'
             },
             link: function link(scope, element) {
+                scope.updateCallbackTime = scope.updateCallbackTime || 1000;
                 var renderArea = element.find('.eeh-flot');
-                eehFlot(renderArea, scope.dataset, scope.options);
+                var chart = eehFlot(renderArea, scope.dataset, scope.options);
+                if (angular.isDefined(scope.updateCallback) &&
+                    angular.isFunction(scope.updateCallback)) {
+                    $interval(function() {
+                        scope.updateCallback(chart, scope.dataset);
+                    }, scope.updateCallbackTime);
+                }
             }
         };
     }
